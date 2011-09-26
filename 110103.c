5908c6df59f94800af56e3efbc140645db27e984
Flávio Juvenal <flaviojuvenal@gmail.com>
2011-09-25 23:48:18 -0300

    correcting the name of the problem

--- 110103.c
+++ 110103.c
@@ -1,5 +1,5 @@
 /*
-Problem A: The Trip
+The Trip
 
 A number of students are members of a club that travels annually to exotic locations. Their destinations in the past have included Indianapolis, Phoenix, Nashville, Philadelphia, San Jose, and Atlanta. This spring they are planning a trip to Eindhoven.
 The group agrees in advance to share expenses equally, but it is not practical to have them share every expense as it occurs. So individuals in the group pay for particular things, like meals, hotels, taxi rides, plane tickets, etc. After the trip, each student's expenses are tallied and money is exchanged so that the net cost to each is the same, to within one cent. In the past, this money exchange has been tedious and time consuming. Your job is to compute, from a list of expenses, the minimum amount of money that must change hands in order to equalize (within a cent) all the students' costs.
