8d02c5706ec3bcedfa20ac66a74843540593cb2c
Flávio Juvenal <flaviojuvenal@gmail.com>
2011-10-14 16:14:29 -0300

    Changing tabs to spaces on 'The 3n + 1 problem'

--- The 3n + 1 problem.c
+++ The 3n + 1 problem.c
@@ -36,29 +36,29 @@ int cycleSize(int x) {
     int cycle = 1;
 
     while (x != 1) {
-	if (x % 2 == 0) { //if even
-	    x = x / 2;
-	} else { //if odd
-	    x = x * 3 + 1;
-	}
-	++cycle;
+        if (x % 2 == 0) { //if even
+            x = x / 2;
+        } else { //if odd
+            x = x * 3 + 1;
+        }
+        ++cycle;
     }
     return cycle;
 }
 
 int maxCycleSizeBetween(int a, int b) {
     if (a > b) { //if b > a, swap them
-	int temp = a;
-	a = b;
-	b = temp;
+        int temp = a;
+        a = b;
+        b = temp;
     }
     int maxCycle = 0;
     
     for (; a <= b; a++) {
-	int thisCycleSize = cycleSize(a);
-	if (thisCycleSize > maxCycle) {
-	    maxCycle = thisCycleSize;
-	}
+        int thisCycleSize = cycleSize(a);
+        if (thisCycleSize > maxCycle) {
+            maxCycle = thisCycleSize;
+        }
     }
     return maxCycle;
 }
@@ -67,7 +67,7 @@ int main() {
     int a, b; //input vars
     
     while (scanf("%d %d", &a, &b) != EOF) {
-	printf("%d %d %d\n", a, b, maxCycleSizeBetween(a, b));
+        printf("%d %d %d\n", a, b, maxCycleSizeBetween(a, b));
     }
     return 0;
 }
